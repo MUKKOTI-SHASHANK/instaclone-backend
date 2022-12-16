2d5d0b3d36334bd014274749d25f93868866c056
MUKKOTI-SHASHANK <mukkoti.shashank@gmail.com>
2022-12-16 18:55:44 +0530

    backend commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const formidable = require("express-formidable");
+const app=express();
+
+require ("dotenv").config();
+const cloudinary = require ("cloudinary").v2;
+if (typeof (process.env.CLOUDINARY_URL) === 'undefined') {
+    console.warn('!! cloudinary config is undefined !!');
+    console.warn('export CLOUDINARY_URL or set dotenv file');
+  } else {
+    console.log('cloudinary config:');
+    console.log(cloudinary.config());
+  }
+  console.log('-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --');
+app.use(formidable());
+const datarouter=require("./routes");
+app.use("/posts",datarouter)
+module.exports=app;
